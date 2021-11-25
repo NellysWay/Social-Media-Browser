03634b5c521cbf5418609c15dc2f5c8bb9277c13
NellyWay <wayabstract20@gmail.com>
2021-11-24 23:49:13 -0500

    Created Stat.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+
+
+const App = () => {
+
+    return (
+        <>
+        <div>
+            <div className="stats-container"></div>
+        </div>       
+         </>
+    );
+}
+
+export default App;
---
+++ src/Stats.js
@@ -0,0 +1,30 @@
+const stats = [
+    {
+        siteName: 'facebook',
+        siteLogo: 'http://www.w3.org/2000/svg',
+        followers: '1987',
+        username: '@realnathenf',
+        newFollowCount: '12 Today',
+    },
+    {
+        siteName: 'twitter',
+        siteLogo: 'http://www.w3.org/2000/svg',
+        followers: '1044',
+        username: '@nathenf',
+        newFollowCount: '99 Today'
+    },
+    {
+        siteName: 'instagram',
+        siteLogo: 'http://www.w3.org/2000/svg',
+        followers: '11k',
+        username: '@realnathanf',
+        newFollowCount: '1099 Today'
+    },
+    {
+        siteName: 'youtube',
+        siteLogo: 'http://www.w3.org/2000/svg',
+        followers: '8239',
+        username: 'Nathan F.',
+        newFollowCount: '144 Today'
+    }
+]
